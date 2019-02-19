91e1548f3d87587221029cd49cb6178d77cbcfd1
Oscar.Chen <chanruida@outlook.com>
2019-02-19 12:55:14 +0800

    init commit
    初始化项目

---
+++ src/main/java/com/withive/hems/HemsApplication.java
@@ -0,0 +1,13 @@
+package com.withive.hems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HemsApplication.class, args);
+    }
+
+}
